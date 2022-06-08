9535c312c9b9206dc6f3e2c8651fa227085b7b93
elfouego <mounpainsaid26@gmail.com>
2022-06-08 16:54:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,13 @@ const functions = require('firebase-functions')
 const express = require("express")
 const app = express();
 const placesRouter = require('./controllers/place_controller')
+const port=3000;
 
 app.use(express.json())
 app.use('/places', placesRouter)
+app.get("/",(req,res)=>res.send("Hello world"));
 
-exports.api = functions.https.onRequest(app)
 
-// To handle "Function Timeout" exception
-exports.functionsTimeOut = functions.runWith({
-    timeoutSeconds: 300
-});
-app.listen(8080, function () { 
-console.log(`Demo project at: 8080!`); });
+app.listen(process.env.PORT|| port,()=>console.log('example app listening at http://localhost:${'+port+'}'));
 
 //exports.setupdb = functions.https.onRequest(require('./setup_database'))
